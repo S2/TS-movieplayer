eb9d196e66fcf84a644d4673d3b5e33c6d49ac7d
sato sin <shinichiro@wano.co.jp>
2014-04-03 17:09:28 +0900

    add DebugConsole

---
+++ src/DebugConsole.ts
@@ -0,0 +1,43 @@
+module Debug{
+    export class Console{
+        console : HTMLDivElement;
+        constructor(){
+        }
+        private create(){
+            if(!this.console){
+                this.console = <HTMLDivElement>document.createElement("Div")
+                var style = this.console.style 
+                style.width  = "200px";
+                style.height = "200px";
+                style.top    = "20px";
+                style.left   = "20px";
+                style.zIndex = "1000000";
+                style.border = "solid 1px black";
+                style.position = "absolute"
+            }
+            document.body.appendChild(this.console);
+        }
+
+        public d(message : string){
+            this.create();
+            if(this.console.innerHTML){
+                this.console.innerHTML += "<br><span style='color:blue'>" + message + "</span>";
+            }else{
+                this.console.innerHTML = "<span style='color:blue'>" + message + "</span>";
+            }
+        }
+
+        public e(message : string){
+            this.create();
+            if(this.console.innerHTML){
+                this.console.innerHTML += "<br><span style='color:red'>" + message + "</span>";
+            }else{
+                this.console.innerHTML = "<span style='color:red'>" + message + "</span>";
+            }
+        }
+
+        public clear(){
+            this.console.innerHTML = "";
+        }
+    }
+}
