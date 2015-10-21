c0c5d69437093627aa2ec0da99fe83ef66970d3e
fushiminaoto <fushimi@wano.co.jp>
2015-10-21 17:17:11 +0900

    IE8用のポリフィル切り捨て

--- src/TSPlayer.ts
+++ src/TSPlayer.ts
@@ -36,8 +36,6 @@ interface Document{
     exitFullscreen()
     mozCancelFullScreen()
     webkitCancelFullScreen()
-    ontouchstart()
-    onwebkitfullscreenchange
 }
 
 class CreateOption{
@@ -571,11 +569,7 @@ class TSPlayer extends AddEvent{
     }
 
     private getComputedStyle(element : HTMLElement) {
-        if(window.getComputedStyle){
-            return getComputedStyle( element , '');
-        }else{
-            return element.currentStyle
-        }
+        return getComputedStyle( element , '');
     }
     
     private createParentDiv(){
